bb2c77e6d33c6b8ba3957039bbfbfd6066c4885f
纳西妲 · Nahida <yanwenz551@gmail.com>
2024-08-04 02:40:31 +0800

    delete(.disabled): deprecated

--- .disabled/dunst/default.nix
+++
@@ -1,7 +0,0 @@
-{ pkgs, ... }: {
-  home.packages = with pkgs; [ libnotify ];
-  services.dunst = {
-    enable = true;
-    settings = import ./config.nix;
-  };
-}
--- .disabled/waybar/default.nix
+++
@@ -1,8 +0,0 @@
-{
-  programs.waybar = {
-    enable = true;
-    systemd.enable = false;
-    style = builtins.readFile ./style.css;
-    settings = import ./config.nix;
-  };
-}
