e3c5dfbc169e5af1e4e7b1ebd66e1dfacc870865
纳西妲 · Nahida <yanwenz551@gmail.com>
2024-08-15 15:57:41 +0800

    Update

--- Makefile
+++ Makefile
@@ -1,2 +1,3 @@
 updata:
+	nixos-rebuild switch --use-remote-sudo --show-trace
 	git add -A; git commit -am "Update"; git push
--- home/nahida/programs/neovim/lua/plugins/lazy.lua
+++ home/nahida/programs/neovim/lua/plugins/lazy.lua
@@ -20,6 +20,7 @@ return {
     { import = "lazyvim.plugins.extras.lang.rust" },
     { import = "lazyvim.plugins.extras.lang.toml" },
     { import = "lazyvim.plugins.extras.lang.yaml" },
+    { import = "lazyvim.plugins.extras.lang.astro" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lsp.neoconf" },
     { import = "lazyvim.plugins.extras.test.core" },
